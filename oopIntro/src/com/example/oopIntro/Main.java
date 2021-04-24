9523f79e7ffdf558968b903d2097d6dc629ff66d
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-04-25 00:09:22 +0300

    Added ProductManager to project.

--- oopIntro/src/com/example/oopIntro/Main.java
+++ oopIntro/src/com/example/oopIntro/Main.java
@@ -25,5 +25,10 @@ public class Main {
         Category category2 = new Category();
         category2.id = 2;
         category2.name = "Ev/Bahçe";
+
+        ProductManager productManager = new ProductManager();
+        productManager.addToCart(product1);
+        productManager.addToCart(product2);
+        productManager.addToCart(product3);
     }
 }
---
+++ oopIntro/src/com/example/oopIntro/ProductManager.java
@@ -0,0 +1,7 @@
+package com.example.oopIntro;
+
+public class ProductManager {
+    public void addToCart(Product product) {
+        System.out.println("Sepete eklendi: " + product.name);
+    }
+}
