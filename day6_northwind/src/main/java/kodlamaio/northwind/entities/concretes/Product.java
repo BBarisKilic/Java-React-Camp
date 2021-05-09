f26a36ced2a9e8e8abd667be034aa2b24d3a0d7b
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-09 19:00:50 +0300

    Learned Autowired of Spring and how API handles requests.

--- day6_northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ day6_northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -23,11 +23,13 @@ public class Product {
     @Column(name = "unit_price")
     private double unitPrice;
 
-    @Column(name = "units_in_price")
+    @Column(name = "units_in_stock")
     private short unitsInStock;
 
     @Column(name = "quantity_per_unit")
     private String quantityPerUnit;
+
+    public Product(){}
 
     public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
         this.id = id;
