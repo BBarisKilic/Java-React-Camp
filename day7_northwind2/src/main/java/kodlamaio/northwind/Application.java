259c3d5d06a3e2eb3f4d56acd860cb1e5ee6bb8f
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-22 22:06:13 +0300

    Added Swagger to project.

---
+++ day7_northwind2/src/main/java/kodlamaio/northwind/Application.java
@@ -0,0 +1,24 @@
+package kodlamaio.northwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("kodlamaio.northwind"))
+				.build();
+	}
+}
