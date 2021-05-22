6d66b2413ad177d5e046360b47b0f60ab53920e9
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-05-22 23:53:28 +0300

    Added constructor to Product class via Lombok.

--- day7_northwind2/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ day7_northwind2/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -1,12 +1,16 @@
 package kodlamaio.northwind.entities.concretes;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Data
 @Entity
 @Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
@@ -29,14 +33,4 @@ public class Product {
     @Column(name = "quantity_per_unit")
     private String quantityPerUnit;
 
-    public Product(){}
-
-    public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock, String quantityPerUnit) {
-        this.id = id;
-        this.categoryId = categoryId;
-        this.productName = productName;
-        this.unitPrice = unitPrice;
-        this.unitsInStock = unitsInStock;
-        this.quantityPerUnit = quantityPerUnit;
-    }
 }
