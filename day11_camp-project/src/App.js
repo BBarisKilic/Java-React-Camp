d62382096d5a4850e5a2184ecb46fd1cad8cce9d
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-05 22:35:13 +0300

    Learned fundamentals of React.

---
+++ day11_camp-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dashboard from './layouts/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard/>
+    </div>
+  );
+}
+
+export default App;
